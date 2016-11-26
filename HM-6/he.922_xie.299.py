558cdbc8aa7da8f65922fae9fc119f423e3988ad
macchiatoyn <shinyyn@gmail.com>
2016-11-25 23:39:33 -0600

    Update he.922_xie.299.py

--- HM-6/he.922_xie.299.py
+++ HM-6/he.922_xie.299.py
@@ -2,8 +2,20 @@ import numpy as np
 import operator
 import sys
 from sklearn.datasets import fetch_olivetti_faces
-data = fetch_olivetti_faces()
-targets = data.target
+from sklearn.model_selection import train_test_split
+
+faces = fetch_olivetti_faces()
+samples,h,w = faces.images.shape
+feature = faces.data
+feature_size = feature.shape[1]
+
+label = faces.target
+label_name = np.unique(label)
+class_size = label_name.shape
+
+print("Number of samples: %d" % samples)
+print("Number of features: %d" % feature_size)
+print("Number of classes: %d" % class_size)
 
 
 def PCA(matrix, num = 64):
@@ -21,7 +33,42 @@ def PCA(matrix, num = 64):
     #print selected_eigen_pairs(eigen_pairs)[0][1].reshape(64, 4096)
 
 
+def softmax(matrix):
+    numerator = np.exp(matrix - np.max(matrix))
+    ans = numerator / numerator.sum()
+    return ans
 
 
-print PCA(data.data)
+def softmaxPredict(matrix,testFeature):
+    """ Compute the class probabilities for each example """
+
+    #matrix = matrix.reshape(class_size, testFeature.shape)
+    dot = np.dot(matrix, testFeature)
+    numerator = np.exp(dot)
+    probabilities = numerator / np.sum(numerator, axis=0)
+
+    predictions = np.zeros((testFeature.shape[1], 1))
+    predictions[:, 0] = np.argmax(probabilities, axis=0)
+
+    return predictions
+
+
+
+trainingFeature, testFeature, trainingLabel, testLabel = train_test_split(
+    feature, label, test_size=0.5, random_state=42)
+
+num_of_train = 20
+print("Extracting the top %d eigenfaces from %d faces"
+      % (num_of_train, trainingFeature.shape[0]))
+
+reducedMatrix = PCA(trainingFeature)
+softmax_regressor = softmax(reducedMatrix)
+#print Softmax_regressor
+
+# TO DO: prediction
+print softmax_regressor.shape
+print testFeature.shape
+#predict_softmax = softmaxPredict(softmax_regressor,testFeature)
+#print predict_softmax
+
 
